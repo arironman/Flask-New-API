7fa93a9bf19b889f1b0a7c4dec17b3afb1da6cbe
Anurag Rai <71434438+arironman@users.noreply.github.com>
2021-12-02 21:14:06 +0530

    Add files via upload

---
+++ old-utils.py
@@ -0,0 +1,33 @@
+from googletrans import Translator
+
+
+# making connection with google translate api by creating its object
+translator = Translator()
+
+
+def get_api_key(path='private/API KEY.txt'):
+    ''''
+        Return the api key of NEW API
+    '''
+    with open(path, 'r') as f:
+        key = f.read()
+    return key
+
+
+def text_translate(text, from_lang='en', to_lang='french'):
+    if not text:
+        text = 'None'
+    translated_text = translator.translate(text, src=from_lang, dest=to_lang)
+    return translated_text.text
+
+
+def sort_article_data(article):
+    new_article = dict()
+    # for data, value in article.items():
+    new_article['title_en'] = article['title']
+    new_article['desc_en'] = article['description']
+    new_article['title_fr'] = text_translate(article['title'])
+    new_article['desc_fr'] = text_translate(article['description'])
+    new_article['timestamp'] = article['publishedAt']
+    new_article['image_url'] = article['urlToImage']
+    return new_article
